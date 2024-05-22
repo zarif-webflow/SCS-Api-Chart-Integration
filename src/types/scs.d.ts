c269f48cd2688541234de408a5ceaeb337dd87da
zarif.webflow@gmail.com <zariftjwr.catalyst@gmail.com>
2024-05-23 05:22:36 +0600

    Scs fetched init with initial global types

---
+++ src/types/scs.d.ts
@@ -0,0 +1,73 @@
+// SevenCleanSeas.d.ts
+
+declare namespace SevenCleanSeasAPI {
+  interface RequestPayload {
+    [key: string]: unknown;
+  }
+
+  interface ResponseResult {
+    result: unknown;
+  }
+
+  interface PlasticCompositionResponse {
+    [key: string]: unknown;
+  }
+
+  interface TopTeamsResponse {
+    [key: string]: unknown;
+  }
+
+  interface TopographyResponse {
+    [key: string]: unknown;
+  }
+
+  interface CollectionActivitiesResponse {
+    [key: string]: unknown;
+  }
+}
+
+declare class SevenCleanSeas {
+  constructor(apiToken: string, isClient: 0 | 1, clientId?: number);
+
+  private createHeaders(apiToken: string, isClient: 0 | 1, clientId?: number): HeadersInit;
+  private sendRequest<T>(endpoint: string, payload: SevenCleanSeasAPI.RequestPayload): Promise<T>;
+
+  scsPeopleHour({ clientId, year }: { clientId: number; year: number }): Promise<number>;
+
+  scsTotalPlasticRemoved(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.PlasticCompositionResponse>;
+  scsPlasticComposition(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.PlasticCompositionResponse>;
+  scsTopTeams(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.TopTeamsResponse>;
+  scsTopography(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.TopographyResponse>;
+  scsCollectionActivities(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.CollectionActivitiesResponse>;
+
+  clientPeopleHour({ clientId, year }: { clientId: number; year: number }): Promise<[number]>;
+  clientTotalPlasticRemoved({
+    clientId,
+    year,
+  }: {
+    clientId: number;
+    year: number;
+  }): Promise<[number]>;
+  clientPlasticComposition(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.PlasticCompositionResponse>;
+  clientTopTeams(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.TopTeamsResponse>;
+  clientTopography(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.TopographyResponse>;
+  clientCollectionActivities(
+    payload: SevenCleanSeasAPI.RequestPayload
+  ): Promise<SevenCleanSeasAPI.CollectionActivitiesResponse>;
+}
---
+++ src/utils/scs-client.ts
@@ -0,0 +1,1 @@
+export const scsClient = new SevenCleanSeas('c0d495db-42de-4eda-810a-213f172dff72', 1, 18);
