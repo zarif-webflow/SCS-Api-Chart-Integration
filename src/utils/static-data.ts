2da5a291a2fc133334ba5a70a815a2c616a33e21
zarif.webflow@gmail.com <zariftjwr.catalyst@gmail.com>
2024-05-22 23:33:35 +0600

    Added some example data prepared for chart js

---
+++ src/utils/static-data.ts
@@ -0,0 +1,101 @@
+import Color from 'color';
+
+export const doughnutChartData = [
+  { color: '#E4F408', valueInPercentage: 31.99, label: 'Apparel & Textile' },
+  { color: '#AB52F2', valueInPercentage: 18.81, label: 'Fishing Lines' },
+  { color: '#BD7BF2', valueInPercentage: 10.77, label: 'Plastic Bags' },
+  { color: '#D0A3F3', valueInPercentage: 10.68, label: 'Bulky Plastic Waste' },
+  {
+    color: '#E3CDF5',
+    valueInPercentage: 7.53,
+    label: 'Product & Logistics Packaging',
+  },
+  {
+    color: '#FC541F',
+    valueInPercentage: 6.03,
+    label: 'Personal Care',
+  },
+  {
+    color: '#FA7C54',
+    valueInPercentage: 2.52,
+    label: 'Tires',
+  },
+  {
+    color: '#F8A48A',
+    valueInPercentage: 2.5,
+    label: 'Plastic Bottles & Bottle Caps',
+  },
+  {
+    color: '#F7CDC0',
+    valueInPercentage: 2.47,
+    label: 'Food Wrapping',
+  },
+  {
+    color: '#0F3382',
+    valueInPercentage: 2.2,
+    label: 'Disposable Cups & Plates',
+  },
+  {
+    color: '#48639F',
+    valueInPercentage: 1.55,
+    label: 'Disposable Food Containers',
+  },
+  {
+    color: '#8294BB',
+    valueInPercentage: 1.06,
+    label: 'Fishing Gear',
+  },
+  {
+    color: '#BCC5D9',
+    valueInPercentage: 0.64,
+    label: 'Appliances & Electronic Cases',
+  },
+  {
+    color: '#FC5671',
+    valueInPercentage: 0.52,
+    label: 'Sachet & Refills',
+  },
+  {
+    color: '#FA7E92',
+    valueInPercentage: 0.3,
+    label: 'Unidentifiable Plastic',
+  },
+  {
+    color: '#F8A5B3',
+    valueInPercentage: 0.28,
+    label: 'Tetra Pak',
+  },
+  {
+    color: '#F7CED4',
+    valueInPercentage: 0.1,
+    label: 'Straws, Stirrers & Cutleries',
+  },
+  {
+    color: '#FFFFFF',
+    valueInPercentage: 0.03,
+    label: 'Stationaries',
+  },
+  {
+    color: '#7A7A7A',
+    valueInPercentage: 0.02,
+    label: 'Cigarette Butts',
+  },
+].map((x) => ({
+  ...x,
+  inactiveColor: Color(x.color).desaturate(0.6).toString(),
+}));
+
+export const graphData = [
+  { month: 'Jan', removedPlasticInKgs: 4600 },
+  { month: 'Feb', removedPlasticInKgs: 4664 },
+  { month: 'Mar', removedPlasticInKgs: 4592 },
+  { month: 'Apr', removedPlasticInKgs: 8029 },
+  { month: 'May', removedPlasticInKgs: 16852 },
+  { month: 'Jun', removedPlasticInKgs: 17026 },
+  { month: 'Jul', removedPlasticInKgs: 15522 },
+  { month: 'Aug', removedPlasticInKgs: 16360 },
+  { month: 'Sep', removedPlasticInKgs: 18180 },
+  { month: 'Oct', removedPlasticInKgs: 20111 },
+  { month: 'Nov', removedPlasticInKgs: 26912 },
+  { month: 'Dec', removedPlasticInKgs: 28205 },
+];
