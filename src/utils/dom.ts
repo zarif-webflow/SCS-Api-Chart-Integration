09309d5d87238f6987ddf371f2dadaca5b249639
zarif-webflow <zarif.webflow@gmail.com>
2024-05-25 02:18:26 +0600

    Removed page wrapper no scroll behaviour

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,12 +1,7 @@
 export const removePageLoader = () => {
-  const pageWrapper = document.querySelector('.page-wrapper');
-  if (!pageWrapper) {
-    throw new Error(".page-wrapper wasn't found!");
-  }
-  const pageLoader = pageWrapper.querySelector('.page-loader');
+  const pageLoader = document.querySelector('.page-loader');
   if (!pageLoader) {
     throw new Error(".page-loader wasn't found!");
   }
-  pageWrapper.classList.remove('is-loading');
   pageLoader.remove();
 };
